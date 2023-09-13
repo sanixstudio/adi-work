fc1ae5eac6fe9b74312c9c6085150e4d4e1fd548
Adnan Niaz <sanixstudio@gmail.com>
2023-09-13 15:37:11 -0700

    converted into pwa

---
+++ public/service-worker.js
@@ -0,0 +1,26 @@
+// service-worker.js
+
+const cacheName = 'my-app-cache-v1';
+const cacheFiles = [
+  '/',
+  '/index.html',
+  '/static/css/main.chunk.css',
+  '/static/js/main.chunk.js',
+  // Add more assets to cache as needed
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/public/service-worker.js") // Adjust the path to your service worker file
+      .then((registration) => {
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.error("Service Worker registration failed:", error);
+      });
+  });
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
