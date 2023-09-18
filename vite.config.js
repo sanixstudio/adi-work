82559174cf33dbf21b66ca480afe8e8e0d95d6ed
Adnan Niaz <sanixstudio@gmail.com>
2023-09-17 23:10:48 -0700

    modified setting to try PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import VitePWA from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// ...
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    reactRefresh(),
+    VitePWA({
+      manifest: {
+        name: "Adi Portfolio",
+        short_name: "Adi-portfolio",
+        description: "Adnan Niaz portfolio with introduction and projects",
+        start_url: "/public/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+      },
+      workbox: {
+        // Options for Workbox (optional)
+      },
+    }),
+  ],
+});
