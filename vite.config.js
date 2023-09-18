76703b50da282751de7bd7e9ddd206ef2a5f26e2
Adnan Niaz <sanixstudio@gmail.com>
2023-09-18 08:15:50 -0700

    changed settings in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,7 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
-import VitePWA from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
-// ...
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    reactRefresh(),
-    VitePWA({
-      manifest: {
-        name: "Adi Portfolio",
-        short_name: "Adi-portfolio",
-        description: "Adnan Niaz portfolio with introduction and projects",
-        start_url: "/public/",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#000000",
-      },
-      workbox: {
-        // Options for Workbox (optional)
-      },
-    }),
-  ],
+  plugins: [react()],
 });
